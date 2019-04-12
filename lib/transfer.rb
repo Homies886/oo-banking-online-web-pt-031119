6e4890e67e861386362432711aae66f279745983
Nicholas Rodriguez <niko876@hotmail.com>
2019-04-12 14:04:20 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -15,23 +15,23 @@ class Transfer
   end  
   
   def execute_transaction
-    if valid? && sender.balance > amount && self.status == "pending"
+    if valid? && sender.balance > amount && @status == "pending"
       sender.balance -= amount
       receiver.balance += amount
-      self.status = "complete"
+      @status = "complete"
     else
-      self.status = "rejected"
+      @status = "rejected"
       "Transaction rejected. Please check your account balance."
     end  
   end
   
   def reverse_transfer
-    if valid? && receiver.balance > amount && self.status == "complete"
+    if valid? && receiver.balance > amount && @status == "complete"
       receiver.balance -= amount
       sender.balance += amount
-      self.status = "reversed"
+      @status = "reversed"
     else
-      self.status = "rejected"
+      @status = "rejected"
     end  
   end  
 end
